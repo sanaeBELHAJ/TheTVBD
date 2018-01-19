9ee04b2135eceb5b93830be009e16da0820294dc
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-01-19 14:37:03 +0100

    Vue /account

--- app/src/main/java/com/example/sanaebelhaj/thetvbd/Views/AccountActivity.java
+++ app/src/main/java/com/example/sanaebelhaj/thetvbd/Views/AccountActivity.java
@@ -6,65 +6,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.Spinner;
 
-import com.example.sanaebelhaj.thetvbd.Services.TheTVDBClient;
-import com.example.sanaebelhaj.thetvbd.Models.TheTVDBRepo;
 import com.example.sanaebelhaj.thetvbd.R;
-import com.example.sanaebelhaj.thetvbd.Views.Adapter.TheTVDBRepoAdapter;
 
-import java.util.Arrays;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AccountActivity extends AppCompatActivity {
-
-    private ListView listView;
-
-    private String[] prenoms = new String[]{
-            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
-            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
-            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
-            "Yann", "Zoé"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_series);
+        setContentView(R.layout.activity_account);
 
-
-        /*Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl("https://api.thetvdb.com/")
-                .addConverterFactory(GsonConverterFactory.create());
-        Retrofit retrofit = builder.build();
-
-        TheTVDBClient client =  retrofit.create(TheTVDBClient.class);
-        */
-
-        //Daredevil ID
-        //Call<List<TheTVDBRepo>> call = client.repoSeries("281662");
-
-        /*call.enqueue(new Callback<List<TheTVDBRepo>>() {
-        @Override
-            public void onResponse(Call<List<TheTVDBRepo>> call, Response<List<TheTVDBRepo>> response) {
-                List<TheTVDBRepo> repos = response.body();
-                listView.setAdapter((ListAdapter) new TheTVDBRepoAdapter(SeriesActivity.this,repos));
-            }
-
-            @Override
-            public void onFailure(Call<List<TheTVDBRepo>> call, Throwable t) {
-                Toast.makeText(SeriesActivity.this, "error :(",Toast.LENGTH_SHORT).show();
-            }
-        });
-        */
+        Spinner dropdown = findViewById(R.id.list_languages);
+        String[] items = new String[]{"English", "Français"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
