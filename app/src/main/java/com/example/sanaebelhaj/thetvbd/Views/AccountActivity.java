de57abc21721d3f468c18c72b1c1a890cb5aec35
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-01-20 20:53:46 +0100

    Methode de récupération des infos User

--- app/src/main/java/com/example/sanaebelhaj/thetvbd/Views/AccountActivity.java
+++ app/src/main/java/com/example/sanaebelhaj/thetvbd/Views/AccountActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,24 +12,93 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.sanaebelhaj.thetvbd.Models.TheTVDBLogin;
+import com.example.sanaebelhaj.thetvbd.Models.TheTVDBToken;
 import com.example.sanaebelhaj.thetvbd.R;
+import com.example.sanaebelhaj.thetvbd.Services.TheTVDBClient;
 
 import java.util.Locale;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class AccountActivity extends AppCompatActivity {
+    private static String token;
+    private static ResponseBody infos;
+    private final String THETVDB_URL_API = "https://api.thetvdb.com";
+
+    Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(THETVDB_URL_API)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    Retrofit retrofit = builder.build();
+    TheTVDBClient userClient = retrofit.create(TheTVDBClient.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        TheTVDBLogin login = new TheTVDBLogin("398D024B39FD5AEE","7367A4BB383FE4A0","Sabertooth28");
+        Call<TheTVDBToken> callLogin = userClient.login(login);
+        getToken(callLogin);
+
+        //List languages
         Spinner dropdown = findViewById(R.id.list_languages);
         String[] items = new String[]{"English", "Français"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
     }
 
+
+    public void getToken(Call<TheTVDBToken> call){
+        call.enqueue(new Callback<TheTVDBToken>() {
+            @Override
+            public void onResponse(Call<TheTVDBToken> call, Response<TheTVDBToken> response) {
+                if(response.isSuccessful()){
+                    token = response.body().getToken();
+                    Log.i("LOGIN","Token => " + token);
+
+                    Call<ResponseBody> callUser = userClient.getUserInfo(token);
+                    getUserInfos(callUser);
+                }
+                else{
+                    Toast.makeText(AccountActivity.this,"error :(",Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<TheTVDBToken> call, Throwable t) {
+                Toast.makeText(AccountActivity.this,"error :(",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void getUserInfos(Call<ResponseBody> call){
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()){
+                    infos = response.body();
+                    Log.i("LOGIN","Infos de l'utilisateur => " + infos);
+                }
+                else{
+                    Toast.makeText(AccountActivity.this,"error :(",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(AccountActivity.this,"error :(",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //Change language
     public void sendFeedback(View v){
         final Spinner feedbackSpinner = (Spinner) findViewById(R.id.list_languages);
         String language = feedbackSpinner.getSelectedItem().toString();
